319c53709720895adcced1cfebb1141bf3c5117d
Eleazar Ramos <e@fureyfs.com>
2020-05-31 12:05:26 -0400

    fixed issue with aceDiamonds

--- components/Cards.js
+++ components/Cards.js
@@ -1,37 +1,41 @@
 const styles = {
   container: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   card: {
     margin: 5,
     height: 100,
     width: 65,
-    border: "1px solid grey",
+    border: '1px solid grey',
     padding: 3,
   },
 }
 
-const PNG_BASE_URL = "https://deckofcardsapi.com/static/img/"
+const PNG_BASE_URL = 'https://deckofcardsapi.com/static/img/'
 
 const Cards = (props) => {
   const { cards, revealed } = props
 
   const cardImageSrc = (card) =>
     revealed
-      ? PNG_BASE_URL + card + ".png"
-      : "https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1535755695"
+      ? PNG_BASE_URL + card + '.png'
+      : 'https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1535755695'
 
   return (
     <div style={styles.container}>
-      {cards.map((card, i) =>
-        card === "" ? (
+      {cards.map((c, i) => {
+        let card = c
+        if (card === 'AD') {
+          c = 'aceDiamonds'
+        }
+        return card === '' ? (
           <div key={i} style={styles.card} />
         ) : (
-          <img key={i} style={styles.card} src={cardImageSrc(card)} />
+          <img key={i} style={styles.card} src={cardImageSrc(c)} />
         )
-      )}
+      })}
     </div>
   )
 }
